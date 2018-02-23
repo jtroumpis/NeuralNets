5453b22af2ac61ccd810959cad085eae676be502
John Troumpis <jtroumpis@gmail.com>
2018-02-23 18:26:11 +0200

    Cosmetic fixes

--- main.py
+++ main.py
@@ -41,18 +41,18 @@ def readCSV(filename = 'data.csv', keep_this=None):
                         d[key] = [value]
         return l
 
+# Calculates the distance
 def calcDistance(k,array,center):
     dist = 0
     for j in range(0,len(array[k])):
         dist += math.pow(array[k][j] - center[j],2)
     return math.sqrt(dist)
 
+# Calculate the maximum distance of each cluster (dmax)
 def multidistance(array,n_clusters,cluster_array,center_array):
     max_dist = {}
     for i in range(0,n_clusters):
         max_dist[i] = 0
-
-    # print(center_array)
 
     for i in range(0,len(array)):
         dist = calcDistance(i,array,center_array[cluster_array[i]])
@@ -60,48 +60,40 @@ def multidistance(array,n_clusters,cluster_array,center_array):
 
     return (max_dist)
 
+
 def listToArrays(input_list):
     item = input_list.pop(0)
     X = np.array([[int(item['CPU']),int(item['MemoryUsed'])]])
     Y = np.array([[int(item['tempCPU'])]])
     for item in input_list:
-        # print(X,[item['CPU'],item['MemoryUsed']])
         if not item['CPU']:
             break
-
         X = np.append(X,[[int(item['CPU']),int(item['MemoryUsed'])]], axis=0)
         Y = np.append(Y,[[int(item['tempCPU'])]], axis=0)
     return X,Y
 
-
-
+# Calculates the gaussian function for a vector
 def gaussianFunction(array, center, sigma):
     gaussian_row = np.array([])
     for k in range(len(array)):
         dist = calcDistance(k, array, center)
-
         fraction = math.pow(dist / sigma,2)
-
         res = math.exp(-fraction)
         gaussian_row = np.append(gaussian_row,[res])
 
     return gaussian_row
 
+# Makes the gaussian matrix
 def gaussianMatrix(array,center_array,sigma_array):
-    # gaussian_array = np.array([[]])
-    # print(center_array)
     for i in range(len(center_array)):
         g = gaussianFunction(array, center_array[i], sigma_array[i])
-        # print('g',g.shape)
         try:
             gaussian_array = np.vstack([gaussian_array, g])
         except UnboundLocalError:
             gaussian_array = np.array(g)
-
-
-    # print('array',gaussian_array.transpose().shape)
     return gaussian_array.transpose()
 
+# Calculate root mean error
 def rootMeanError(error_array):
     souma = 0
     for i in error_array:
@@ -109,12 +101,23 @@ def rootMeanError(error_array):
 
     return math.sqrt(souma/len(error_array))
 
-train, test = (readCSV(keep_this=['CPU','MemoryUsed','tempCPU']))
-# print(d)
+def kMeans(trainX,n_clusters):
+    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans.fit(trainX)
+    y_kmeans = kmeans.predict(trainX)
+    centers = kmeans.cluster_centers_
+
+    return y_kmeans, centers
 
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()  # for plot styling
 import numpy as np
+from sklearn.cluster import KMeans
+from numpy.linalg import inv
+
+n_clusters = 3
+
+train, test = (readCSV(keep_this=['CPU','MemoryUsed','tempCPU']))
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -122,18 +125,7 @@ ax = fig.add_subplot(111)
 trainX, trainY = listToArrays(train)
 testX, testY = listToArrays(test)
 
-n_clusters = 3
-# print(X)
-
-# plt.show()
-
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=n_clusters)
-kmeans.fit(trainX)
-y_kmeans = kmeans.predict(trainX)
-centers = kmeans.cluster_centers_
-
-
+y_kmeans, centers = kMeans(trainX, n_clusters)
 
 dmaxes = multidistance(trainX,n_clusters,y_kmeans,centers)
 
@@ -141,7 +133,7 @@ sigma_array = dmaxes
 for i in sigma_array:
     i = 2/3*(i)
 
-G = (gaussianMatrix(trainX,centers,sigma_array))
+G = gaussianMatrix(trainX,centers,sigma_array)
 
 print("G dimensions: ",G.shape)
 
@@ -154,23 +146,21 @@ temp = gamma.dot(gamma.transpose())
 
 to_inverse = np.add(to_inverse, temp)
 
-print('G=', G)
-
-from numpy.linalg import inv
+# print('G=', G)
 
 inversed = inv(to_inverse)
 
-print("Inversed = ",inversed)
-print("Inversed*to_inverse = ",inversed.dot(to_inverse))
+# print("Inversed = ",inversed)
+# print("Inversed*to_inverse = ",inversed.dot(to_inverse))
 
 temp = inversed.dot(G.transpose())
 W = temp.dot(trainY)
 
-print("W=",W)
+# print("W=",W)
 
 Y = G.dot(W)
 
-print("Y=",Y)
+# print("Y=",Y)
 
 error = np.subtract(trainY,Y)
 
