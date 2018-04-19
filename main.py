bb485d571911f780995570a1b3141637aa8e61f8
John Troumpis <jtroumpis@gmail.com>
2018-04-19 16:26:04 +0300

    Added main menu

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-import math
+# import math
 from utilities import *
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()  # for plot styling
+# import matplotlib.pyplot as plt
+# import seaborn as sns; sns.set()  # for plot styling
 import numpy as np
-from numpy.linalg import inv
+# from numpy.linalg import inv
+import pso
 
-lamda = 1
+print("(1). Polynomial RBF with PSO for (centers, sigmas).")
+print("(2). RBF with PSO for (centers, sigmas, weights).")
+selection = int(input("Please choose an option: "))
 
 x, y = readCSV('data.csv')
 x = np.asarray(x)
-print(np.amin(x, axis=0))
-print(np.amax(x, axis=0))
 y = np.asarray(y)
+
+if selection==1:
+    print("Starting Polynomial RBF...")
+    pso.PSO_PRBF(x,y)
+elif selection==2:
+    print("Starting RBF swarm...")
+
+elif selection==3:
+    print("Feed-Forward under construction...")
+
+# print(str(sys.argv))
+#
+# exit()
+
+# lamda = 1
+
+
 
 # print(x.shape)
 # for lamda in [0.1,1,10,100]:
 #     print("LAMDA=",lamda)
-for c in range(2,30):
-    doThePolyNet(lamda,c,x,y)
+# for c in range(2,30):
+#     doThePolyNet(lamda,c,x,y)
